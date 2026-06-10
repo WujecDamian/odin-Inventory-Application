d7fc3ae3b7dae1dac9aa48312736bcdae9ee3f30
Damian Wujec <wujecdamian12@gmail.com>
2026-06-10 14:48:53 +0200

    chore: initialize Express app with basic setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
