70785e993f7b9bf2ef4c21c8259ad1ffb77b05a0
Damian Wujec <wujecdamian12@gmail.com>
2026-06-12 11:59:41 +0200

    fix: update database connection string and add SSL configuration

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,10 @@
 // db/pool.js
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_EXT,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
