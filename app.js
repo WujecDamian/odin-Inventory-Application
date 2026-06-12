0c1fb5d4b75460cfe9cdf0fe00306b9e858199c9
Damian Wujec <wujecdamian12@gmail.com>
2026-06-12 21:21:24 +0200

    refactor: move cloudinary config to own file

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
-//Configure Cloudinary
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  secure: true,
-});
-
-console.log(cloudinary.config());
-
 //Routes
 const homeRoute = require("./Routes/homeRoutes");
 const subcategoryRoute = require("./Routes/subcategoriesRoutes");
