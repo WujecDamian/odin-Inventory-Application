1faeae016ffcbd9f99746186249963e6e8c8a212
Damian Wujec <wujecdamian12@gmail.com>
2026-06-13 14:27:01 +0200

    feat: add functions to create categories, subcategories, and parts

--- Models/queries.js
+++ Models/queries.js
@@ -5,6 +5,14 @@ async function getCategories() {
   return rows;
 }
 
+async function getCategory(name) {
+  const { rows } = await pool.query(
+    "SELECT * FROM categories WHERE name = $1",
+    [name],
+  );
+  return rows[0];
+}
+
 async function getSubcategories(category) {
   const { rows } = await pool.query(
     "SELECT * FROM subcategories WHERE category_id = (SELECT id FROM categories WHERE name = ($1))",
@@ -27,10 +35,30 @@ async function getAllParts() {
   const { rows } = await pool.query("SELECT * FROM parts");
   return rows;
 }
+
+//INSERTS
+
+async function createCategory(name, image) {
+  const { rows } = await pool.query("SELECT * FROM categories");
+  return rows;
+}
+async function createSubcategory(name, image) {
+  const { rows } = await pool.query("SELECT * FROM subcategories");
+  return rows;
+}
+async function createPart(name, image) {
+  const { rows } = await pool.query("SELECT * FROM parts");
+  return rows;
+}
+
 module.exports = {
   getCategories,
+  getCategory,
   getSubcategories,
   getAllSubcategories,
   getParts,
   getAllParts,
+  createCategory,
+  createSubcategory,
+  createPart,
 };
