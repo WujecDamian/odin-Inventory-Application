c8b6d5e28f18669ae381782dd6954f25f0a3e184
Damian Wujec <wujecdamian12@gmail.com>
2026-06-13 14:27:47 +0200

    feat: add form for subcategories with proper select options form db

--- Controllers/subcategoriesController.js
+++ Controllers/subcategoriesController.js
@@ -3,11 +3,25 @@ const db = require("../Models/queries");
 const getSubcategories = async (req, res) => {
   if (!req.query.category) {
     const subcategories = await db.getAllSubcategories();
-    res.render("subcategories", { subcategories });
+    const categories = await db.getCategories();
+    res.render("subcategories", {
+      subcategories,
+      categories,
+      categoryObj: undefined,
+    });
   } else {
     const category = req.query.category;
+    const categoryObj = await db.getCategory(category);
+    console.log(typeof category);
+    console.log(
+      `category: ${categoryObj} | name: ${categoryObj.name} | id: ${categoryObj.id}`,
+    );
     const subcategories = await db.getSubcategories(category);
-    res.render("subcategories", { subcategories });
+    res.render("subcategories", {
+      subcategories,
+      categoryObj,
+      categories: undefined,
+    });
   }
 };
 
