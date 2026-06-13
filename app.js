5447d4ffa3061c79ef53f5108f7b50ccd4672803
Damian Wujec <wujecdamian12@gmail.com>
2026-06-13 16:55:11 +0200

    feat: update app.js to include body-parser middleware for enhanced request handling

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 //Routes
