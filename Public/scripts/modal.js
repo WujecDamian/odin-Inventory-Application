a3f0908e5dbc5b4258abb6d97019a60822c5dd09
Damian Wujec <wujecdamian12@gmail.com>
2026-06-15 13:41:12 +0200

    feat: add type: "module" and use import inside modal.js

--- Public/scripts/modal.js
+++ Public/scripts/modal.js
@@ -1,10 +1,10 @@
+import cloudinaryController from "../../Controllers/cloudinaryController";
+
 const addBtn = document.querySelector(".add__category__btn");
 const closeBtn = document.querySelector(".close__btn");
 const submitBtn = document.querySelector(".submit__btn");
 const modal = document.querySelector(".form__modal");
 const imageUrl = "";
-
-const cloudinaryController = require("../../Controllers/cloudinaryController");
 
 addBtn.addEventListener("click", () => {
   modal.style.display = "block";
