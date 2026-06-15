059cb95086cf4a136113314e8e37a744ff269979
Damian Wujec <wujecdamian12@gmail.com>
2026-06-15 17:11:28 +0200

    feat: Add rows Editing functionality (CRUD <- UPDATE) for parts

--- Controllers/partsController.js
+++ Controllers/partsController.js
@@ -9,12 +9,31 @@ const getParts = async (req, res) => {
   } else {
     const subcategory = req.query.subcategory;
     const parts = await db.getParts(subcategory);
+    const allSubcategories = await db.getAllSubcategories();
     const subcategoryObj = await db.getSubcategory(req.query.subcategory);
 
-    res.render("parts", { parts, subcategoryObj, subcategories: undefined });
+    res.render("parts", {
+      parts,
+      subcategoryObj,
+      allSubcategories,
+      subcategories: undefined,
+    });
   }
+};
+
+const editPart = async (req, res) => {
+  const partId = req.params.id;
+  const brand = req.body.brand;
+  const model = req.body.model;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+  const subcategoryId = req.body.subcategoryId;
+  console.log(partId, brand, model, price, quantity, subcategoryId);
+  await db.updatePart(brand, model, price, quantity, subcategoryId, partId);
+  res.redirect(req.get("Referrer"));
 };
 
 module.exports = {
   getParts,
+  editPart,
 };
--- Routes/partsRoutes.js
+++ Routes/partsRoutes.js
@@ -7,4 +7,6 @@ const partsController = require("../Controllers/partsController");
 // define the home page route
 router.get("/", partsController.getParts);
 
+router.post("/edit/:id", partsController.editPart);
+
 module.exports = router;
